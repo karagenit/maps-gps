67c4ba3701e851fac1355b16f7b8ac59688b3bdf
Derek123454 <dmfronek@gmail.com>
2019-01-02 20:55:54 -0500

    Update map.php

--- map.php
+++ map.php
@@ -10,15 +10,23 @@
     }
     fclose($handle);
   }
+   $filename = 'data.log';
+if (file_exists($filename)) {
+    $timestamp = "Last Updated " . date("F d Y H:i:s.", filemtime($filename));
+}
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Simple Map</title>
+    <title>CHS Rocket Team Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
     <style>
+	div.a{
+			text-align: center;
+			
+	}
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
       #map {
@@ -32,7 +40,26 @@
       }
     </style>
   </head>
-  <body>
+  <body onload="startTime()">
+  <div class="a">
+	<h1 style="font-family:verdana">CHS Rocket Team Location</h1>
+	<table style="margin:1em auto;" border="1" cellpadding="10" cellspacing="0">
+	<tr>
+      <th>Rocket</th>
+	  <th>Status</th>
+      <th>Latitude</th>
+	  <th>Longitude</th>
+	  <th>Time</th>
+   </tr>
+   <tr>
+		<td>Rocket #01</td>
+		<td><?=$timestamp?></td>
+		<td><?=$lat?></td>
+		<td><?=$lng?></td>
+		<td><div id="txt"></div></td>
+	</tr>
+	</table>
+	</div>
     <div id="map"></div>
     <script>
       var rocketLocation = {lat: <?=$lat?>, lng: <?=$lng?> };
@@ -43,8 +70,23 @@
         });
         var marker = new google.maps.Marker({position: rocketLocation, map: map});
       }
+	function startTime() {
+		var today = new Date();
+		var h = today.getHours();
+		var m = today.getMinutes();
+		var s = today.getSeconds();
+		m = checkTime(m);
+		s = checkTime(s);
+		document.getElementById('txt').innerHTML =
+		h + ":" + m + ":" + s;
+		var t = setTimeout(startTime, 500);
+	}
+	function checkTime(i) {
+		if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+		return i;
+	}
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=<?php file_get_contents('api.key') ?>&callback=initMap"
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAteDbXDNBJq6UmK43dtyAdY1pJHXDjS5k<?php file_get_contents?>&callback=initMap"
             async defer></script>
   </body>
 </html>
